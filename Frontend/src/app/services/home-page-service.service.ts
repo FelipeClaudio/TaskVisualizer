2538aed4b0a5d8747f5136305a62e8003872be75
Felipe Santos <felipesantos956@poli.ufrj.br>
2024-10-30 23:43:11 +0000

    chore: added temporary solution to connect frontend with backend.

--- Frontend/src/app/services/home-page-service.service.ts
+++ Frontend/src/app/services/home-page-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import {IUsersResponse } from '../models/IUsersResponse';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class HomePageServiceService {
   constructor(private http: HttpClient) { }
 
   // This needs to be moved to a configuration.
-  private taskVisualizerBackend = 'http://localhost:32123/users';
+  private taskVisualizerBackend = environment.backendAddress;
 
   getUsers(): Observable<IUsersResponse[]> {
-    return this.http.get<IUsersResponse[]>(this.taskVisualizerBackend);
+    return this.http.get<IUsersResponse[]>(this.taskVisualizerBackend + '/users');
   }
 }
