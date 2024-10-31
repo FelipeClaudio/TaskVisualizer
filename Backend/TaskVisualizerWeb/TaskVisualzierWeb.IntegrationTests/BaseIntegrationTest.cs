1b27bbe1c080b122c5d3b99ec1fe841b82925a7a
Felipe Santos <felipesantos956@poli.ufrj.br>
2024-10-31 08:58:33 +0000

    chore: base setup for integration tests - WIP.

---
+++ Backend/TaskVisualizerWeb/TaskVisualzierWeb.IntegrationTests/BaseIntegrationTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.Extensions.DependencyInjection;
+using TaskVisualizerWeb.Repository;
+using TaskVisualzierWeb.IntegrationTests;
+
+public abstract class BaseIntegrationTest
+    : IClassFixture<IntegrationTestWebAppFactory>,
+      IDisposable
+{
+    private readonly IServiceScope _scope;
+    protected readonly EfCorePostgreContext DbContext;
+
+    protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+
+        DbContext = _scope.ServiceProvider
+            .GetRequiredService<EfCorePostgreContext>();
+    }
+
+    public void Dispose()
+    {
+        _scope?.Dispose();
+        DbContext?.Dispose();
+    }
+}
---
+++ Backend/TaskVisualizerWeb/TaskVisualzierWeb.IntegrationTests/IntegrationTestWebAppFactory.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using TaskVisualizerWeb.Repository;
+using Testcontainers.PostgreSql;
+
+namespace TaskVisualzierWeb.IntegrationTests;
+
+public class IntegrationTestWebAppFactory
+    : WebApplicationFactory<Program>,
+      IAsyncLifetime
+{
+    // "Host=localhost;Port=32321; Database=TaskVisualizerDB; Username=ps_user; Password=SecurePassword"
+    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+        .WithDatabase("TaskVisualizerDB-IntegrationTest")
+        .WithUsername("ps_user")
+        .WithPassword("testpassword")
+        .WithImage("postgres:14")
+        .Build();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            var descriptorType =
+                typeof(DbContextOptions<EfCorePostgreContext>);
+
+            var descriptor = services
+                .SingleOrDefault(s => s.ServiceType == descriptorType);
+
+            if (descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<EfCorePostgreContext>(options => options.UseNpgsql(_dbContainer.GetConnectionString()));
+        });
+    }
+
+    public Task InitializeAsync()
+    {
+        return _dbContainer.StartAsync();
+    }
+
+    public new Task DisposeAsync()
+    {
+        return _dbContainer.StopAsync();
+    }
+}
