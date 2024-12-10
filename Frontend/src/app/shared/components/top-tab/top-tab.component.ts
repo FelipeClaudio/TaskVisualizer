2c004b33af8a510a5f754c6b8aef481cc23bca54
Felipe Santos <felipesantos956@poli.ufrj.br>
2024-12-10 00:39:52 +0000

    feat: changed top tab to use svg icons.

--- Frontend/src/app/shared/components/top-tab/top-tab.component.ts
+++ Frontend/src/app/shared/components/top-tab/top-tab.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { Component, inject } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-top-tab',
     templateUrl: './top-tab.component.html',
     styleUrls: ['./top-tab.component.scss'],
-    imports: [MatTabsModule]
+    imports: [MatTabsModule, MatIconModule]
 })
 
-export class TopTabComponent {}
+export class TopTabComponent {
+    constructor() {
+        const iconRegistry = inject(MatIconRegistry);
+        const sanitizer = inject(DomSanitizer);
+        iconRegistry.addSvgIcon('task-icon', sanitizer.bypassSecurityTrustResourceUrl("../../../assets/task-icon.svg"));
+        iconRegistry.addSvgIcon('reward-icon', sanitizer.bypassSecurityTrustResourceUrl("../../../assets/reward-icon.svg"));
+        iconRegistry.addSvgIcon('calendar-icon', sanitizer.bypassSecurityTrustResourceUrl("../../../assets/calendar-icon.svg"));
+    }
+}
